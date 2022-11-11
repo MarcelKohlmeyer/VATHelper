6bd1979a76be49ff3d02bee7bdd388703a93c664
Marcel Kohlmeyer <marcel@geistesblitz-software.de>
2022-11-11 20:54:37 +0100

    Created solution and boilerplate

---
+++ VATHelper/Controllers/TransactionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VATHelper.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransactionController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetTransactions()
+        {
+            return new OkObjectResult("TransactionController works!");
+        }
+    }
+}
+
